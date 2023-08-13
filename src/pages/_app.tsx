fe4bcfd094a0204050d31cccf484b614354c3327
Aung Khant Zaw <aungkhantzaw133@gmail.com>
2023-08-13 16:37:25 +0630

    todo ui and form setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,44 +3,40 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 // import "~/styles/globals.css";
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider } from "@mantine/core";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
-  pageProps: { session, ...pageProps },
+  // pageProps: { session, ...pageProps },
+  pageProps: { ...pageProps },
 }) => {
   return (
     <MantineProvider
       withGlobalStyles
       withNormalizeCSS
       theme={{
-        colorScheme: 'light',
-        colors: {
-          // Add your color
-          deepBlue: ['#E9EDFC', '#C1CCF6', '#99ABF0' /* ... */],
-          // or replace default theme color
-          blue: ['#E9EDFC', '#C1CCF6', '#99ABF0' /* ... */],
-        },
+        components: {
+          Button: {
+            defaultProps: {
+              size: "xs",
+              color: "blue",
+            },
+          },
 
-        shadows: {
-          md: '1px 1px 3px rgba(0, 0, 0, .25)',
-          xl: '5px 5px 3px rgba(0, 0, 0, .25)',
-        },
-
-        headings: {
-          fontFamily: 'Roboto, sans-serif',
-          sizes: {
-            h1: { fontSize: '2rem' },
+          Switch: {
+            defaultProps: {
+              size: "xs",
+              onLabel: "ON",
+              offLabel: "OFF",
+            },
           },
         },
       }}
-
     >
-<SessionProvider session={session}>
+      {/* <SessionProvider session={session}> */}
       <Component {...pageProps} />
-    </SessionProvider>
+      {/* </SessionProvider> */}
     </MantineProvider>
-    
   );
 };
 
